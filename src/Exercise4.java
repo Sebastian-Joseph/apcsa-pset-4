f74539dcf9a4f55a194e3596b0c0b26c9024de12
Sebastian Joseph <theclimax25@gmail.com>
2020-09-30 00:19:24 -0400

    fifth

---
+++ src/Exercise4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        Scanner centi = new Scanner(System.in);
+        DecimalFormat deci = new DecimalFormat("0");
+
+        System.out.println("Centimeters : ");
+        double cen = centi.nextDouble();
+
+        double kilometers = cen / 100000;
+        double left1 = cen % 100000;
+
+        double meters = left1 / 100;
+        double leftovercentimeters = left1 % 100;
+
+        System.out.println("Miles: " + deci.format(kilometers));
+        System.out.println("Feet: " + deci.format(meters));
+        System.out.println("Inches: " + deci.format(leftovercentimeters));
+    }
+}
