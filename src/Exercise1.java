5973bd7a38da49680189985b0deb111cd052f0c5
Sebastian Joseph <theclimax25@gmail.com>
2020-09-29 23:43:48 -0400

    first commit

---
+++ src/Exercise1.java
@@ -0,0 +1,24 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        Scanner weather = new Scanner(System.in);
+        DecimalFormat deci = new DecimalFormat("0.00");
+
+        System.out.println("Temperature: ");
+        double temp = weather.nextDouble();
+
+
+        System.out.println("Wind Speed: ");
+        double windspeed = weather.nextDouble();
+
+        double a = .6215*temp;
+        double b = .4275*temp;
+        double c = Math.pow(windspeed, .16);
+
+        double windchill = 35.74 + a + (b - 35.75) * c;
+
+        System.out.println("Wind Chill: " + deci.format(windchill));
+    }
+}
---
+++ src/Exercise2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        Scanner inches = new Scanner(System.in);
+
+        System.out.println("Inches: ");
+        double in = inches.nextDouble();
+
+        double feet = 
+    }
+}
