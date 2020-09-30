27fd6790d8c4c6ce9ef5b03a990b615164d54838
Sebastian Joseph <theclimax25@gmail.com>
2020-09-30 00:33:22 -0400

    seventh

---
+++ src/Exercise6.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Exercise6 {
+    public static void main(String[] args) {
+        Scanner rect = new Scanner(System.in);
+        DecimalFormat deci = new DecimalFormat("0.00");
+
+        System.out.println("Length : ");
+        double length = rect.nextDouble();
+
+        System.out.println("Width : ");
+        double width = rect.nextDouble();
+
+       double area = length * width;
+       double perimeter = (length + width) * 2;
+       double diagonal = Math.sqrt(length * length + width * width);
+
+        System.out.println("Area: " + deci.format(area));
+        System.out.println("Perimeter: " + deci.format(perimeter));
+        System.out.println("Diagonal: " + deci.format(diagonal));
+    }
+}
